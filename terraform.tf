e28a8ecd85dde6ff9c801e4a8e0f29a61f863efc
Denny Vettom <dennysv@gmail.com>
2023-09-07 12:53:51 +0100

    Update by DV on Thu  7 Sep 2023 12:53:51 BST

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,12 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "github-action-aws-tf"
+    key            = "github-action-aws-tf/terraform.tfstate"
+    region         = "eu-west-1"
+    encrypt        = true
+    dynamodb_table = "github-action-aws-tf"
+    profile        = "dvettom"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,5 +15,6 @@ terraform {
   }
 }
 provider "aws" {
-  region = "eu-west-1"
+  region  = "eu-west-1"
+  profile = "dvettom"
 }
